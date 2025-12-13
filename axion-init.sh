2747fcfca075f508c224e9a0e862f95c58bce217
Renan <renanmvc421@gmail.com>
2025-12-13 00:42:14 -0300

    update

--- axion-init.sh
+++ axion-init.sh
@@ -1,3 +1,3 @@
-yay -S hyprland hyprlock hyprpaper hyprpolkitagent sddm man tldr thunar python3 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl ttf-google-sans ttf-google-sans-code-nf --noconfirm
+yay -S hyprland hyprlock hyprpaper hyprpolkitagent sddm man tldr thunar python3 unzip 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl noto-fonts-emoji rclone --noconfirm
 
 echo "Rembember to install starhip and setup sddm :)"
