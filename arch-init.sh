bb01d4996396a7471a9de59bf52775ee715a093b
Renan <renanmvc421@gmail.com>
2026-02-07 09:39:43 -0300

    update

--- arch-init.sh
+++ arch-init.sh
@@ -1,3 +1,3 @@
-yay -S hyprland hyprlock hyprpaper hyprpolkitagent pipewire pipewire-pulse pavucontrol intel-ucode nvdia nvidia-utils nvidia-dkms starship sddm man tldr thunar python3 unzip 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl noto-fonts-emoji rclone flatpak obsidian baobab xdg-desktop-portal-hyprland xdg-desktop-portal-gtk --noconfirm
+yay -S hyprland hyprlock hyprpaper hyprshot hyprpolkitagent pipewire pipewire-pulse pavucontrol intel-ucode nvdia nvidia-utils nvidia-dkms starship sddm man tldr thunar python3 unzip 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl noto-fonts-emoji rclone flatpak obsidian baobab xdg-desktop-portal-hyprland xdg-desktop-portal-gtk ripgrep --noconfirm
 
 echo "Rembember to install starhip and setup sddm (SilentSDDM) :)"
