79704b20e451a5a469a9687f36734bb4353ef808
Renan <renanmvc421@gmail.com>
2026-02-17 16:56:42 -0300

    updating scripts

--- arch-init.sh
+++ arch-init.sh
@@ -1,3 +1,4 @@
-yay -S hyprland hyprlock hyprpaper hyprshot hyprpolkitagent pipewire pipewire-pulse pavucontrol intel-ucode nvdia nvidia-utils nvidia-dkms starship sddm man tldr thunar python3 unzip 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl noto-fonts-emoji rclone flatpak obsidian baobab xdg-desktop-portal-hyprland xdg-desktop-portal-gtk ripgrep --noconfirm
+#!/bin/bash
+yay -S hyprland hyprlock hyprpaper hyprshot hyprpolkitagent pipewire pipewire-pulse pavucontrol intel-ucode nvdia nvidia-utils nvidia-dkms starship sddm man tldr thunar python3 zip unzip 7zip jdk-openjdk nvim qemu-full waybar rofi wofi neofetch firefox kitty ttf-jetbrains-mono-nerd openssh stow brightnessctl noto-fonts-emoji rclone flatpak obsidian baobab xdg-desktop-portal-hyprland xdg-desktop-portal-gtk ripgrep --noconfirm
 
 echo "Rembember to install starhip and setup sddm (SilentSDDM) :)"
---
+++ cyber-tools.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Obs: I'm using BlackArch Repo to install the tools!
+sudo pacman -S nmap amass recon-ng maltego burpsuite zaproxy gobuster sqlmap wfuzz metasploit exploitdb hashcat john hydra wireshark-qt dnsenum
